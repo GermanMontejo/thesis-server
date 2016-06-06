516e962fbd752504852736487dae82423d999355
VanEvangelista <ivand.evangelista@gmail.com>
2016-06-06 18:13:33 +0800

    Add files via upload
    With changes to fit current database

--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,7 @@ package utils
 
 import (
 	"database/sql"
-	. "github.com/GermanMontejo/thesis-server/model"
+	. "github.com/thesis-server/model"
 	"log"
 	_ "github.com/lib/pq"
 	"fmt"
@@ -12,27 +12,28 @@ var pgsql PgSQL
 
 const (
 	DB_USER     = "postgres"
-	DB_PASSWORD = "postgres"
-	DB_NAME     = "web_server_thesis"
+	DB_PASSWORD = "thesis123"
+	DB_NAME     = "thesis"
 )
 
 func InitializeDB() *PgSQL{
 
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
+		DB_USER, DB_PASSWORD,  DB_NAME)
+
+	log.Println("dbinfo:", dbinfo)
 
 	if pgsql.SQL == nil {
 		db, err := sql.Open("postgres", dbinfo)
-		defer db.Close()
 		if err != nil {
 			log.Fatal("Error while trying to open a db connection:", err)
 		}
 		log.Println("db stats:", db.Stats())
 		pgsql = PgSQL{db}
 
-		_, err = db.Exec(`CREATE TABLE students(id serial, firstname text, lastname text, course text, year int, section string, mac_address string)`)
+		_, err = db.Exec(`CREATE TABLE roomrequest_user2(userid serial, firstname text, lastname text, idnum text, pass text, schoolyear text, term int, roomrequest text, dlsuemail text, assignroom text, status text, contactnum int, mac_address text)`)
 		if err  != nil {
-			log.Fatal("Error while creating a table:", err)
+			log.Println("Error while creating a table:", err)
 		}
 	}
 	return &pgsql
