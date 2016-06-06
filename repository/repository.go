b59e53fcb2842fa64948346e09bf194253dedfd7
VanEvangelista <ivand.evangelista@gmail.com>
2016-06-06 18:12:49 +0800

    Add files via upload
    with changes to fit current database

--- repository/repository.go
+++ repository/repository.go
@@ -1,7 +1,7 @@
 package repository
 
 import (
-	. "github.com/GermanMontejo/thesis-server/model"
+	. "github.com/thesis-server/model"
 	"log"
 )
 
@@ -11,10 +11,14 @@ type Repository struct {
 
 func (repo *Repository) InsertStudent(student *Student) error {
 	sql := repo.SQL
-	rows, err := sql.Query("INSERT INTO students(firstname, lastname, course, year, section, mac_address) VALUES($1, $2, $3, $4, $5, $6)", student.Firstname, student.Lastname, student.Course, student.Year, student.Section, student.MacAddress)
+	res, err := sql.Exec("INSERT INTO roomrequest_user2(firstname, lastname, idnum, pass, schoolyear, term, dlsuemail, status, contactnum, mac_address) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", student.Id, student.Firstname, student.Lastname, student.Idnum, student.Pass, student.Schoolyear, student.Term, student.RoomRequest, student.Dlsuemail, student.Assignroom, student.Status, student.Contactnum, student.MacAddress)
 	if err != nil {
 		log.Println("Error inserting data:", err)
 		return err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Println("Error reading rows affected:", err)
 	}
 	log.Println("Inserted", rows, "record(s).")
 	return nil
@@ -22,16 +26,21 @@ func (repo *Repository) InsertStudent(student *Student) error {
 
 func (repo *Repository) GetStudents() []Student {
 	sql := repo.SQL
+	student := new(Student)
 	students := []Student{}
-	rows, err := sql.Query("SELECT * FROM students")
+	rows, err := sql.Query("SELECT * FROM roomrequest_user2")
 	if err != nil {
 		log.Println("Error retrieving student data:", err)
 		return students
 	}
-	err = rows.Scan(&sql)
-	if err != nil {
-		log.Println("Error retrieving data set:", err)
-		return []Student{}
+	for rows.Next() {
+		err = rows.Scan(&student.Id, &student.Firstname, &student.Lastname, &student.Idnum, &student.Pass, &student.Schoolyear, &student.Term, &student.RoomRequest, &student.Dlsuemail, &student.Assignroom, &student.Status, &student.Contactnum, &student.MacAddress)
+		if err != nil {
+			log.Println("Error retrieving data set:", err)
+			return []Student{}
+		}
+		students = append(students, *student)
 	}
+	log.Println(student)
 	return students
 }
